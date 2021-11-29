8cf228b39148d3dd293bd11242f09ed90c8a9991
jungukjung <wjddnr944@naver.com>
2021-11-29 17:36:43 +0900

    슬라이드 효과 어색한 부분 수정!

--- jk_cp_display_app/static/ts/base.js
+++ jk_cp_display_app/static/ts/base.js
@@ -1,10 +1,12 @@
 function side_bar_up() {
     $("#side_bar_up").css("width", "0");
+    $("#side_bar_up").css("z-index", "0");    
     $("#side_bar_up").css("visibility", "hidden");
-    $("#side_bar_up").css("transition", "all .25s");
+    $("#side_bar_up").css("transition", "all .3s");
 }
 function side_bar_down() {
     $("#side_bar_up").css("width", "330");
+    $("#side_bar_up").css("z-index", "20");
     $("#side_bar_up").css("visibility", "visible");
-    $("#side_bar_up").css("transition", "all .25s");
+    $("#side_bar_up").css("transition", "all .3s");
 }
--- jk_cp_display_app/static/ts/base.ts
+++ jk_cp_display_app/static/ts/base.ts
@@ -1,16 +1,15 @@
 function side_bar_up() {
     $("#side_bar_up").css("width","0")
+    $("#side_bar_up").css("transition","all .25s")
     $("#side_bar_up").css("visibility","hidden")
-    $("#side_bar_up").css("transition","all .25s")  
+  
 }
 
 
 function side_bar_down() {
     $("#side_bar_up").css("width","330")
     $("#side_bar_up").css("visibility","visible")
-    $("#side_bar_up").css("transition","all .25s") 
-    
-    
+    $("#side_bar_up").css("transition","all .25s")     
 }
 
 
