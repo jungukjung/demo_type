a5d53e45643c24f58a3c33fde00b3eb45512e2be
jungukjung <wjddnr944@naver.com>
2021-11-29 17:08:21 +0900

    [ 2021-11-29 ] 슬라이드 효과 적용 !

--- jk_cp_display_app/static/ts/base.js
+++ jk_cp_display_app/static/ts/base.js
@@ -1,8 +1,10 @@
 function side_bar_up() {
-    $("#side_bar_up").css("display", "none");
-    $("#side_bar_down").css("display", "block");
+    $("#side_bar_up").css("width", "0");
+    $("#side_bar_up").css("visibility", "hidden");
+    $("#side_bar_up").css("transition", "all .25s");
 }
 function side_bar_down() {
-    $("#side_bar_up").css("display", "block");
-    $("#side_bar_down").css("display", "none");
+    $("#side_bar_up").css("width", "330");
+    $("#side_bar_up").css("visibility", "visible");
+    $("#side_bar_up").css("transition", "all .25s");
 }
