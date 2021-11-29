5ada1d3932c41afff72e97bd5fe836210c935db7
jungukjung <wjddnr944@naver.com>
2021-11-29 18:26:42 +0900

    사이드 효과 어색한 부분 다시 수정 및 폴더정리

--- jk_cp_display_app/static/ts/base.js
+++ jk_cp_display_app/static/ts/base.js
@@ -1,12 +1,16 @@
 function side_bar_up() {
+    $(".side_txt").css("display", "none");
     $("#side_bar_up").css("width", "0");
     $("#side_bar_up").css("z-index", "0");
-    $("#side_bar_up").css("transition", "all .3s");
     $("#side_bar_up").css("visibility", "hidden");
+    $("#side_bar_up").css("transition", "all 1.3s");
 }
 function side_bar_down() {
     $("#side_bar_up").css("width", "330");
     $("#side_bar_up").css("z-index", "20");
     $("#side_bar_up").css("visibility", "visible");
-    $("#side_bar_up").css("transition", "all .3s");
+    $("#side_bar_up").css("transition", "all 1.3s");
+    setTimeout(function () {
+        $(".side_txt").css("display", "block");
+    }, 1000);
 }
